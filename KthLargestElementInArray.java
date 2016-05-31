a5f17b5de59d4a80e767787e8160aa5a9c469dad
Eugene <ernestyj@outlook.com>
2016-05-31 09:40:09 +0800

    update

--- KthLargestElementInArray.java
+++ KthLargestElementInArray.java
@@ -9,8 +9,14 @@ import java.util.Arrays;
  */
 public class KthLargestElementInArray {
 
+    /**方法总结:
+     * 堆:时间 O(NlogK) 空间 O(K)
+     * 先排序:时间 O(NlogN) 空间 O(1)(其实空间不止常数项)
+     * 快速选择:时间 Avg O(N) Worst O(N^2) 空间 O(1)
+     */
 
-    class Solu{//https://segmentfault.com/a/1190000003704825
+    //https://segmentfault.com/a/1190000003704825
+    class Solu{
         public int findKthLargest(int[] nums, int k) {
             return quickSelect(nums, k-1, 0, nums.length - 1);
         }
@@ -18,19 +24,19 @@ public class KthLargestElementInArray {
         private int quickSelect(int[] arr, int k, int left, int right){
             int pivot = arr[(left + right) / 2];
             int i = left, j = right;
-            while(i <= j){
-                while(arr[i] > pivot) i++;  // 从左向右找到第一个小于枢纽值的数
-                while(arr[j] < pivot) j--;  // 从右向左找到第一个大于枢纽值的数
-                if(i <= j){ // 将两个数互换
+            while(i<=j){
+                while(arr[i]>pivot) i++;  // 从左向右找到第一个小于枢纽值的数
+                while(arr[j]<pivot) j--;  // 从右向左找到第一个大于枢纽值的数
+                if(i<=j){ // 将两个数互换
                     swap(arr, i, j);
                     i++;
                     j--;
                 }
             }// 最后退出的情况应该是右指针在左指针左边一格
             // 这时如果右指针还大于等于k，说明kth在左半边
-            if(left < j && k <= j) return quickSelect(arr, k, left, j);
+            if(left<j && k<=j) return quickSelect(arr, k, left, j);
             // 这时如果左指针还小于等于k，说明kth在右半边
-            if(i < right && k >= i) return quickSelect(arr, k, i, right);
+            if(i<right && k>=i) return quickSelect(arr, k, i, right);
             return arr[k];
         }
         private void swap(int[] a, int i, int j) {
